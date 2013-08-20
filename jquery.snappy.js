bbccfdbf3bd091bccf126362f4a30a4df41b4604
tjtokar <tj@higherimages.com>
2013-08-20 15:49:25 -0400

    Create jquery.snappy.js

---
+++ jquery.snappy.js
@@ -0,0 +1,94 @@
+$.fn.snappyScroll = function(scrollDuration, startAtTop) {
+	
+	var activeSection = 0;
+	var isAnimating = false;
+	var lastLayer = parseInt($(".layer").length);
+	
+	function layerChecker(){
+		var layerCounter = 1;
+		$(".layer").each( function() {
+			atTop = $(this).offset().top - $(window).scrollTop();
+			if( atTop == 0 ){
+				$(".layer:nth-of-type("+layerCounter+")").attr('data-start', 'start');
+				activeSection = layerCounter - 1;
+			}
+			layerCounter++;
+		});
+	}
+	
+	layerChecker();
+	
+	window.addEventListener('DOMMouseScroll', onMouseWheel, false);
+	window.onmousewheel = document.onmousewheel = onMouseWheel;
+	
+	function onMouseWheel(event) {
+	    event.stopPropagation();
+	    event.preventDefault();
+	    event.cancelBubble = true;
+	    if(isAnimating == true){
+			return false;
+		}
+		var FF = (document.getBoxObjectFor != null || window.mozInnerScreenX != null);
+		if (FF) {
+			if( event.detail < 0 ){
+				direction = "up";
+			} else {
+				direction = "down";
+			}
+		} else {
+			if( event.wheelDelta > 0 ){
+				direction = "up";
+			} else {
+				direction = "down";
+			}
+		}
+
+		scrollsnapper(event);
+	    return false;
+	}
+	
+	function scrollsnapper(event){
+		
+		
+		if( direction == "up"){
+			activeSection--;
+		} else {
+			activeSection++;
+		}
+        if( activeSection < 0 ){
+	        activeSection = 0;
+	        return false;
+        }
+        
+        if( activeSection > lastLayer ){
+	        activeSection = lastLayer;
+	        return false;
+        }
+		
+		if( direction == "down") {
+			nextLayer = $(".layer[data-start=start]").removeAttr('data-start').next().offset().top;
+		} else {
+			nextLayer = $(".layer[data-start=start]").removeAttr('data-start').prev().offset().top;
+		}
+		console.log(event.wheelDelta);
+		
+		
+		
+		scrollToSection(nextLayer);
+	}
+	
+	function scrollToSection( abs ){
+		if(isAnimating == true){
+			return false;
+		}
+		
+		isAnimating = true;
+		
+		$("html, body").animate({ scrollTop: abs }, scrollDuration, function() {
+			 isAnimating = false;
+			 layerChecker();
+		});
+	}
+
+
+}
